876db077fcf41f2aa473e4f4ba4b6f71d0cd6d95
kpawlik <kamil.pawlik@capgemini.com>
2023-06-05 21:41:02 +0200

    Updating MNIST dataset with TFJS utils

--- src/data/mnist.ts
+++ src/data/mnist.ts
@@ -1,3 +1,5 @@
+import * as tf from "@tensorflow/tfjs";
+
 // images properties
 const IMAGE_X = 28;
 const IMAGE_Y = 28;
@@ -20,53 +22,41 @@ export async function loadMNIST(ratio = 0.8) {
 	if (ratio > 0.9) ratio = 0.9;
 
 	// calc dataset sizes
-	const numTrain = Math.round(NUM_ELEMENTS * ratio);
-	const numTest = NUM_ELEMENTS - numTrain;
-	const train = {
-		inputs: new Array<Float32Array>(numTrain),
-		labels: new Array<Uint8Array>(numTrain),
+	const numTrainElements = Math.floor(NUM_ELEMENTS * ratio);
+	const numTestElements = NUM_ELEMENTS - numTrainElements;
+
+	// shuffle datasets indeces
+	const shuffledTrainIndices = tf.util.createShuffledIndices(numTrainElements);
+	const shuffledTestIndices = tf.util.createShuffledIndices(numTestElements);
+
+	// split datasets
+	const trainInputs = shuffledTrainIndices.slice(0, numTrainElements * IMAGE_SIZE);
+	const trainLabels = shuffledTestIndices.slice(0, numTrainElements * NUM_CLASSESS);
+	const testInputs = shuffledTrainIndices.slice(numTrainElements * IMAGE_SIZE);
+	const testLabels = shuffledTestIndices.slice(numTrainElements * NUM_CLASSESS);
+
+	// controlers
+	let shuffledTrainIndex = 0;
+	let shuffledTestIndex = 0;
+
+	return {
+		nextTrainBatch(batchSize: number) {
+			return loadBatch(batchSize, [trainInputs, trainLabels], () => {
+				shuffledTrainIndex += 1;
+				shuffledTrainIndex %= shuffledTrainIndices.length;
+
+				return shuffledTrainIndices[shuffledTrainIndex];
+			});
+		},
+		nextTestBatch(batchSize: number) {
+			return loadBatch(batchSize, [testInputs, testLabels], () => {
+				shuffledTestIndex += 1;
+				shuffledTestIndex %= shuffledTestIndices.length;
+
+				return shuffledTestIndices[shuffledTestIndex];
+			});
+		},
 	};
-
-	const test = {
-		inputs: new Array<Float32Array>(numTest),
-		labels: new Array<Uint8Array>(numTest),
-	};
-
-	// prepare controll variables
-	let numTrainItems = 0;
-	let numTestItems = 0;
-
-	for (let i = 0; i < NUM_ELEMENTS; i++) {
-		const dataset = (() => {
-			// special cases
-			if (NUM_ELEMENTS - i <= numTrain - numTrainItems) return train;
-			if (NUM_ELEMENTS - i <= numTest - numTestItems) return test;
-
-			// default case
-			return Math.random() < ratio ? train : test;
-		})();
-
-		const numItems = dataset === train ? numTrainItems : numTestItems;
-
-		dataset.inputs[numItems] = new Float32Array(IMAGE_SIZE);
-		dataset.labels[numItems] = new Uint8Array(NUM_CLASSESS);
-
-		for (let j = 0; j < IMAGE_SIZE; j++) {
-			dataset.inputs[numItems][j] = inputs[IMAGE_SIZE * i + j];
-		}
-
-		for (let j = 0; j < NUM_CLASSESS; j++) {
-			dataset.labels[numItems][j] = labels[NUM_CLASSESS * i + j];
-		}
-
-		if (dataset === train) {
-			numTrainItems++;
-		} else {
-			numTestItems++;
-		}
-	}
-
-	return [train, test];
 }
 
 // inputs loader
@@ -81,7 +71,7 @@ async function loadInputs(): Promise<Float32Array> {
 		// process loaded image
 		img.addEventListener("load", () => {
 			const datasetBuff = new ArrayBuffer(NUM_ELEMENTS * IMAGE_SIZE * 4);
-			const batchSize = 1000;
+			const batchSize = 5000;
 
 			// resize image
 			img.width = img.naturalWidth;
@@ -124,4 +114,34 @@ async function loadLabels(): Promise<Uint8Array> {
 	const res = new Uint8Array(await req.arrayBuffer());
 
 	return res;
+}
+
+// dataset loaser
+function loadBatch(
+	batchSize: number,
+	datasets: [Uint32Array, Uint32Array],
+	getIndex: () => number
+) {
+	const inputs = new Float32Array(batchSize * IMAGE_SIZE);
+	const labels = new Uint8Array(batchSize * NUM_CLASSESS);
+
+	for (let i = 0; i < batchSize; i++) {
+		const index = getIndex();
+
+		// slice data item
+		const image = datasets[0].slice(index * IMAGE_SIZE, index * IMAGE_SIZE + IMAGE_SIZE);
+		const label = datasets[0].slice(
+			index * NUM_CLASSESS,
+			index * NUM_CLASSESS + NUM_CLASSESS
+		);
+
+		// append data item
+		inputs.set(image, i * IMAGE_SIZE);
+		labels.set(label, i * NUM_CLASSESS);
+	}
+
+	return {
+		inputs: tf.tensor2d(inputs, [batchSize, IMAGE_SIZE]),
+		labels: tf.tensor2d(labels, [batchSize, NUM_CLASSESS]),
+	};
 }
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,5 @@
 import { loadMNIST } from "./data/mnist";
 
-const cvs = document.createElement("canvas");
-const ctx = cvs.getContext("2d")!;
-
-cvs.width = 28;
-cvs.height = 28;
-
-document.body.append(cvs);
-
-loadMNIST().then(([train, test]) => {
-	const inputIndex = Math.floor(Math.random() * train.inputs.length);
-	const inputData = new Uint8ClampedArray(train.inputs[inputIndex].length * 4);
-
-	// process picture
-	for (let i = 0; i < train.inputs[inputIndex].length; i++) {
-		inputData[4 * i + 0] = train.inputs[inputIndex][i] * 255;
-		inputData[4 * i + 1] = train.inputs[inputIndex][i] * 255;
-		inputData[4 * i + 2] = train.inputs[inputIndex][i] * 255;
-		inputData[4 * i + 3] = 255;
-	}
-
-	// process image
-	const imageData = new ImageData(inputData, 28, 28);
-
-	// draw image
-	ctx.putImageData(imageData, 0, 0);
+loadMNIST().then((dataset) => {
+	console.log(dataset.nextTrainBatch(20));
 });
